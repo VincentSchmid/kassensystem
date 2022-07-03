c4dcad36918ee860fc20d1a63b9817db1480205b
Vincent Schmid <Vincent.Schmid@ti8m.ch>
2022-07-03 17:57:32 +0200

    allowed hosts using domain name env

--- backend/django-server/core/settings.py
+++ backend/django-server/core/settings.py
@@ -34,8 +34,14 @@ SECRET_KEY = "django-insecure-yjon%!*t6=x9-+3$h#j$_)2d@@!9=u(nzq-0w1b&7i@!8s)9-!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
+print(f"DEBUG: {DEBUG}")
 
-ALLOWED_HOSTS = ["*"]
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = [env("DOMAIN_NAME")]
+
+print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
 # Application definition
 
